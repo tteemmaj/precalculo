f792944f36b491b5e22b5973d7116e929cc53f92
Juan Témaj <epro639@gmail.com>
2025-08-26 15:17:16 -0600

    pagina de inicio y correción de estilos

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,23 @@
+import "../styles/Cards.css"
+import {Link} from "react-router-dom"
+import Dot from "../assets/arrow.svg"
+
 function Cards(props) {
   return(
     <>
-      <Link to={props.link}> 
-        <div clasName="padreTarjeta"></div> 
+      <Link className="linkCardsStyle" to={props.link}> 
+        <div className="padreTarjeta">
+          <div className="temas">
+              <p> <img src={ Dot } className="Dot" /> { props.tema1 } </p>
+              <p> <img src={ Dot } className="Dot" />  { props.tema2 } </p>
+              <p> <img src={ Dot } className="Dot" /> { props.tema3 }</p>
+          </div>
+          <div className="moduloBoton">
+            <div className="logo"> { <img className="logoTema" src={ props.logoTema } />  }</div>
+            <div className="nombre"> <h3> { props.numModulo } </h3> </div>
+            <div className="boton"> <h4>Ver resumen </h4> </div>
+          </div>
+        </div> 
       </Link>
     </>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,40 @@
 import "../styles/Home.css"
 import Cards from "../components/Cards.jsx"
+import equaLogo from "../assets/equa.png"
+import funLogo from "../assets/function.svg"
+import anglesLogo from "../assets/angles.svg"
 
 function Home() {
   return(
     <>
       <div className="mainDiv">
         <div><h1 className="tituloGeneral">Resumen Precálculo 2025</h1></div>
+        <div className="divCards">
+          <Cards 
+            link ="/moduloI" 
+            tema1 = "Números reales, exponentes y radicales."
+            tema2 = "Expresiones algebraicas y racionales."
+            tema3 = "Ecuaciones y desigualdades."
+            numModulo = "modulo I"
+            logoTema = { equaLogo }
+          /> 
+          <Cards 
+            link ="/moduloII" 
+            tema1 = "Geometría y rectas."
+            tema2 = "Funciones gráficas y transformaciones."
+            tema3 = "Funciones cuadráticas."
+            numModulo = "modulo II"
+            logoTema = { funLogo }
+          />
+          <Cards 
+            link ="/moduloI" 
+            tema1 = "Funciones polimoniales y números complejos"
+            tema2 = "Trigonometría y ángulos"
+            tema3 = "Ley de senos y cosenos"
+            numModulo = "modulo III"
+            logoTema = { anglesLogo }
+          /> 
+        </div>
       </div>
     </>
   )
